10db8c6310dbb00a2d2f640304bb715b675b4ab9
Mariusz Serwin <mariooserwin@gmail.com>
2016-04-21 17:02:47 +0100

    CleanUp for AutoScalling

---
+++ Files/CleanUP.ps1
@@ -0,0 +1,12 @@
+﻿$OctopusURL = "http://octopusdsc" #Your Octopus URL
+$OctopusAPIKey = "API-ORT9F3DASQOL6HXPCOTEOZXDG " #Your Octopus API Key
+$header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
+
+
+
+$machine = (Invoke-WebRequest $OctopusURL/api/machines/all -Headers $Header -UseBasicParsing ).content | ConvertFrom-Json | ?{$_.Name -eq $env:COMPUTERNAME}
+$machine  = $machine | ? {$_.name -eq $env:COMPUTERNAME }
+
+
+
+Invoke-WebRequest $OctopusURL/api/machines/$($machine.Id)  -Method Delete -Headers $header
